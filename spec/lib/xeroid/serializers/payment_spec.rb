189362dbd90884150c803082b07af13230169ba1
Matt Patterson <matt@reprocessed.org>
2012-11-17 14:03:21 +0100

    Only run the Schema validation specs under JRuby

--- spec/lib/xeroid/serializers/payment_spec.rb
+++ spec/lib/xeroid/serializers/payment_spec.rb
@@ -9,31 +9,33 @@ require 'nokogiri'
 require 'pathname'
 
 module Xeroid::Serializers
-  describe Account do
-    def schema_dir
-      Pathname.new(File.expand_path("../../../../../vendor/XeroAPI-Schemas/v2.00", __FILE__))
-    end
+  if RUBY_PLATFORM == 'java'
+    describe Account do
+      def schema_dir
+        Pathname.new(File.expand_path("../../../../../vendor/XeroAPI-Schemas/v2.00", __FILE__))
+      end
 
-    before(:all) do
-      @validator = Nokogiri::XML::Schema(File.open(schema_dir.join("Payment.xsd")))
-    end
+      before(:all) do
+        @validator = Nokogiri::XML::Schema(File.open(schema_dir.join("Payment.xsd")))
+      end
 
-    let(:account) { ::Xeroid::Objects::Account.new(code: "NWBC") }
-    let(:invoice) { ::Xeroid::Objects::Invoice.new(id: "abcdef12-1234-abcd-1234-abcdef123456") }
+      let(:account) { ::Xeroid::Objects::Account.new(code: "NWBC") }
+      let(:invoice) { ::Xeroid::Objects::Invoice.new(id: "abcdef12-1234-abcd-1234-abcdef123456") }
 
-    it "produces a valid XML document given a single, minimal, Account object" do
-      payment = ::Xeroid::Objects::Payment.new(invoice: invoice, account: account, amount: BigDecimal.new("10"), date: Date.parse("2012-11-15"))
-      xml = Payment.serialize(payment)
-      @validator.valid?(Nokogiri::XML(xml)).should be_true
-    end
-
-    context "payments for a foreign currency invoice" do
       it "produces a valid XML document given a single, minimal, Account object" do
-        payment = ::Xeroid::Objects::Payment.new(invoice: invoice, account: account, 
-                                                 amount: BigDecimal.new("10"), date: Date.parse("2012-11-15"),
-                                                 currency_rate: BigDecimal.new("1.2"))
+        payment = ::Xeroid::Objects::Payment.new(invoice: invoice, account: account, amount: BigDecimal.new("10"), date: Date.parse("2012-11-15"))
         xml = Payment.serialize(payment)
         @validator.valid?(Nokogiri::XML(xml)).should be_true
+      end
+
+      context "payments for a foreign currency invoice" do
+        it "produces a valid XML document given a single, minimal, Account object" do
+          payment = ::Xeroid::Objects::Payment.new(invoice: invoice, account: account, 
+                                                   amount: BigDecimal.new("10"), date: Date.parse("2012-11-15"),
+                                                   currency_rate: BigDecimal.new("1.2"))
+          xml = Payment.serialize(payment)
+          @validator.valid?(Nokogiri::XML(xml)).should be_true
+        end
       end
     end
   end
