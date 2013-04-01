ee2316ee8ff8528806504ed8c0424491bd2838f6
Matt Patterson <matt@reprocessed.org>
2013-04-01 22:31:55 +0200

    Add LineItem to xeroid/objects.rb auto loading

--- lib/xeroid/objects.rb
+++ lib/xeroid/objects.rb
@@ -3,6 +3,7 @@ module Xeroid
     autoload :Account, 'xeroid/objects/account'
     autoload :Contact, 'xeroid/objects/contact'
     autoload :Invoice, 'xeroid/objects/invoice'
+    autoload :LineItem, 'xeroid/objects/line_item'
     autoload :Payment, 'xeroid/objects/payment'
   end
 end
