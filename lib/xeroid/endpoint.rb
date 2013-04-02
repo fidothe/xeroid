8d2efe4fe873ff73cc4d6bbb1431b611d047bd9d
Matt Patterson <matt@reprocessed.org>
2013-04-02 12:04:11 +0200

    Break out HTTP request-making; introduce the concept of a deserialiser to process responses

--- lib/xeroid/endpoint.rb
+++ lib/xeroid/endpoint.rb
@@ -1,13 +1,21 @@
 module Xeroid
   class Endpoint
-    def initialize(auth_token, path)
+    def initialize(auth_token, path, methods, deserialiser)
       @auth_token = auth_token
       @path = path
-      @methods = methods
+      @allowed_methods = methods
+      @deserialiser = deserialiser
     end
 
     def all
-      @auth_token.get("/api.xro/2.0/#{@path}")
+      raise HTTPMethodNotAllowed if !@allowed_methods.include?(:get)
+      @deserialiser.process_many(fetch_response)
+    end
+
+    def fetch_response(method)
+      @auth_token.send(method, "/api.xro/2.0/#{@path}")
     end
   end
+
+  class HTTPMethodNotAllowed < StandardError; end
 end
--- spec/xeroid/endpoint_spec.rb
+++ spec/xeroid/endpoint_spec.rb
@@ -4,15 +4,36 @@ require 'xeroid/endpoint'
 
 module Xeroid
   describe Endpoint do
-    let(:stub_auth) { stub('Xeroid::Auth') }
+    let(:stub_token) { stub('Xeroid::Auth::Private') }
     let(:stub_response) { stub('Response') }
+    let(:stub_deserialiser) { stub('Xeroid::Deserialisers::Thing') }
+    let(:stub_result) { stub('Xeroid::Objects::Thing') }
     let(:api_path_prefix) { '/api.xro/2.0' }
 
-    it "can make a GET request to an endpoint" do
-      endpoint = Endpoint.new(stub_auth, 'Endpoint')
-      stub_auth.should_receive(:get).with("#{api_path_prefix}/Endpoint").and_return(stub_response)
+    describe "making the HTTP request with the Auth'd access token" do
+      let(:endpoint) { Endpoint.new(stub_token, 'Endpoint', [], stub_deserialiser) }
 
-      endpoint.all().should == stub_response
+      it "can correctly form the URL for a fetch-many request" do
+        stub_token.should_receive(:get).with("#{api_path_prefix}/Endpoint").and_return(stub_response)
+
+        endpoint.fetch_response(:get).should == stub_response
+      end
+    end
+
+    describe "making GET requests" do
+      it "cannot make the request if GET is not an allowed HTTP method" do
+        endpoint = Endpoint.new(stub_token, 'Endpoint', [], stub_deserialiser)
+        expect { endpoint.all }.to raise_error(HTTPMethodNotAllowed)
+      end
+
+      it "deserialises the response correctly for a fetch-many call" do
+        endpoint = Endpoint.new(stub_token, 'Endpoint', [:get], stub_deserialiser)
+        endpoint.stub(:fetch_response).and_return(stub_response)
+
+        stub_deserialiser.should_receive(:process_many).with(stub_response).and_return([stub_result])
+
+        endpoint.all.should == [stub_result]
+      end
     end
   end
 end
