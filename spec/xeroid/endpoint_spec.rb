c1dcf438d23f890e4305bc60b92a5894ee60fb24
Matt Patterson <matt@reprocessed.org>
2013-04-02 11:00:21 +0200

    tiny bit of editorial clean up

--- spec/xeroid/endpoint_spec.rb
+++ spec/xeroid/endpoint_spec.rb
@@ -6,10 +6,11 @@ module Xeroid
   describe Endpoint do
     let(:stub_auth) { stub('Xeroid::Auth') }
     let(:stub_response) { stub('Response') }
+    let(:api_path_prefix) { '/api.xro/2.0' }
 
     it "can make a GET request to an endpoint" do
       endpoint = Endpoint.new(stub_auth, 'Endpoint')
-      stub_auth.should_receive(:get).with('/api.xro/2.0/Endpoint').and_return(stub_response)
+      stub_auth.should_receive(:get).with("#{api_path_prefix}/Endpoint").and_return(stub_response)
 
       endpoint.all().should == stub_response
     end
