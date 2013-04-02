2b17b4dc0273f4cc99136c4fe4d16f43ea6fa4a6
Matt Patterson <matt@reprocessed.org>
2013-04-02 15:55:28 +0200

    Bring the client spec and code in line with what'd been done to Endpoint

--- lib/xeroid/client.rb
+++ lib/xeroid/client.rb
@@ -7,7 +7,7 @@ module Xeroid
     end
 
     def invoices
-      Endpoint.new(@auth_token, 'Invoice', [:get, :post, :put], [:id, :invoice_number], [:modified_after, :where, :order])
+      Endpoint.new(@auth_token, 'Invoice', [:get, :post, :put], Deserialisers::Invoice, Serialisers::Invoice)
     end
   end
 end
--- spec/xeroid/client_spec.rb
+++ spec/xeroid/client_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 require 'xeroid/client'
 
 module Xeroid
+  module Serialisers
+    class Invoice; end
+  end
+
+  module Deserialisers
+    class Invoice; end
+  end
+
   describe Client do
     let(:endpoint) { stub('Endpoint')  }
     let(:stub_auth) { stub('Auth') }
 
     it "can correctly create an Endpoint for dealing with the /Invoice endpoint" do
       Endpoint.should_receive(:new)
-        .with(stub_auth, 'Invoice', [:get, :post, :put], [:id, :invoice_number], [:modified_after, :where, :order])
+        .with(stub_auth, 'Invoice', [:get, :post, :put], Deserialisers::Invoice, Serialisers::Invoice)
         .and_return(endpoint)
 
       client = Client.new(stub_auth)
