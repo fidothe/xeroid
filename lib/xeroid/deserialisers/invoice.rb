8495836625fd81a662bf8e3e70d2c3a55d5c07e7
Matt Patterson <matt@reprocessed.org>
2013-04-04 18:32:15 +0200

    Add several attributes to the Invoice deserialiser
    Added:
    * type
    * date
    * due_date
    * status
    * sub_total

--- lib/xeroid/deserialisers/invoice.rb
+++ lib/xeroid/deserialisers/invoice.rb
@@ -1,9 +1,20 @@
 require 'xeroid/objects'
 require 'nokogiri'
+require 'date'
 
 module Xeroid
   module Deserialisers
     class Invoice
+      TYPES = {'ACCREC' => Xeroid::Objects::Invoice::Type::ACCREC,
+               'ACCPAY' => Xeroid::Objects::Invoice::Type::ACCPAY}
+      STATUSES = {
+        'DRAFT' => Xeroid::Objects::Invoice::Status::DRAFT,
+        'SUBMITTED' => Xeroid::Objects::Invoice::Status::SUBMITTED,
+        'AUTHORISED' => Xeroid::Objects::Invoice::Status::AUTHORISED,
+        'VOIDED' => Xeroid::Objects::Invoice::Status::VOIDED,
+        'DELETED' => Xeroid::Objects::Invoice::Status::DELETED,
+        'PAID' => Xeroid::Objects::Invoice::Status::PAID
+      }
       def self.deserialise_one(xml)
         doc = Nokogiri::XML(xml)
         new(doc).deserialise
@@ -13,11 +24,33 @@ module Xeroid
         @document = document
       end
 
+      def extract_text(xpath)
+        @document.xpath(xpath).text
+      end
+
+      def extract_value(xpath, values)
+        values[extract_text(xpath)]
+      end
+
+      def extract_date(xpath)
+        date_string = extract_text(xpath)
+        Date.parse(date_string)
+      end
+
+      def extract_currency(xpath)
+        BigDecimal.new(extract_text(xpath))
+      end
+
       def deserialise
         attributes = {}
         # core attributes
-        attributes[:id] = @document.xpath('/Invoices/Invoice/InvoiceID').text
-        
+        attributes[:id] = extract_text('/Invoices/Invoice/InvoiceID')
+        attributes[:type] = extract_value('/Invoices/Invoice/Type', TYPES)
+        attributes[:date] = extract_date('/Invoices/Invoice/Date')
+        attributes[:due_date] = extract_date('/Invoices/Invoice/DueDate')
+        attributes[:status] = extract_value('/Invoices/Invoice/Status', STATUSES)
+        attributes[:sub_total] = extract_currency('/Invoices/Invoice/SubTotal')
+
         Objects::Invoice.new(attributes)
       end
     end
--- spec/xeroid/deserialisers/invoice_spec.rb
+++ spec/xeroid/deserialisers/invoice_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 require 'xeroid/deserialisers/invoice'
+require 'bigdecimal'
 
 module Xeroid::Deserialisers
   describe Invoice do
@@ -10,6 +11,39 @@ module Xeroid::Deserialisers
       it "correctly extracts the id" do
         result.id.should == "243216c5-369e-4056-ac67-05388f86dc81"
       end
+
+      it "correctly extracts the invoice type" do
+        result.type.should == Xeroid::Objects::Invoice::Type::ACCREC
+      end
+
+      it "correctly extracts the date" do
+        result.date.should == Date.new(2009, 5, 27)
+      end
+
+      it "correctly extracts the due date" do
+        result.due_date.should == Date.new(2009, 06, 06)
+      end
+
+      it "correctly extracts the status" do
+        result.status.should == Xeroid::Objects::Invoice::Status::AUTHORISED
+      end
+
+      it "correctly extracts the line amount types" do
+        result.line_amount_types.should == Xeroid::Objects::Invoice::LineAmounts::EXCLUSIVE
+      end
+
+      it "correctly extracts the sub total" do
+        result.sub_total.should == BigDecimal.new("1800.00")
+      end
+    # <TotalTax>225.00</TotalTax>
+    # <Total>2025.00</Total>
+    # <UpdatedDateUTC>2009-08-15T00:18:43.457</UpdatedDateUTC>
+    # <CurrencyCode>NZD</CurrencyCode>
+    # <InvoiceNumber>OIT00546</InvoiceNumber>
+    # <AmountDue>1025.00</AmountDue>
+    # <AmountPaid>1000.00</AmountPaid>
+    # <AmountCredited>0.00</AmountCredited>
+
     end
   end
 end
