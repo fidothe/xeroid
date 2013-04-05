125976b7ac7f3563b95b3a937fa3d99c29cab271
Matt Patterson <matt@reprocessed.org>
2013-04-05 14:18:39 +0200

    Add several-mapping case to mappings specs, remove extra specs from value-specific mapping specs

--- spec/xeroid/deserialisers/extractors_spec.rb
+++ spec/xeroid/deserialisers/extractors_spec.rb
@@ -38,6 +38,17 @@ module Xeroid::Deserialisers
           attributes[:thing].should == 'A string'
         end
 
+        context "many-value mappings" do
+          let(:xml) { '<r><a>A string</a><b>B string</b></r>' }
+
+          it "can extract several values" do
+            attributes = {}
+            x.extract_from_mapping({:a => '/r/a', :b => '/r/b'}, :string, attributes)
+            attributes[:a].should == 'A string'
+            attributes[:b].should == 'B string'
+          end
+        end
+
         it "ignores empty-result xpaths" do
           attributes = {}
           x.extract_from_mapping({:thing => '/r/null'}, :string, attributes)
@@ -55,70 +66,19 @@ module Xeroid::Deserialisers
     describe "straight-text attributes" do
       let(:xml) { '<r><a>A string</a><b>B string</b></r>' }
       let(:klass) { Class.new { include Extractors; object_class OpenStruct; as_string :thing => '/r/a' } }
-      let(:instance) { klass.deserialise_one(xml) }
 
       it "can extract a value" do
-        instance.thing.should == "A string"
-      end
-
-      context "several values" do
-        let(:klass) { Class.new { include Extractors; object_class OpenStruct; as_string :a => '/r/a', :b => '/r/b' } }
-
-        it "can extract the first value" do
-          instance.a.should == 'A string'
-        end
-
-        it "can extract the second value" do
-          instance.b.should == 'B string'
-        end
-      end
-
-      context "missing values" do
-        let(:klass) { Class.new { include Extractors; object_class OpenStruct; as_string :a => '/r/a', :c => '/r/c' } }
-
-        it "extracts the first value" do
-          instance.a.should == 'A string'
-        end
-
-        it "doesn't attempt to set the second" do
-          instance.should_not respond_to(:c)
-        end
+        klass.deserialise_one(xml).thing.should == "A string"
       end
     end
 
     describe "currency attributes" do
       let(:xml) { '<r><a>10.00</a><b>20.00</b></r>' }
       let(:klass) { Class.new { include Extractors; object_class OpenStruct; as_currency :thing => '/r/a' } }
-      let(:instance) { klass.deserialise_one(xml) }
       let(:ten) { BigDecimal.new("10.00")  }
-      let(:twenty) { BigDecimal.new("20.00") }
 
       it "can extract a value" do
-        instance.thing.should == ten
-      end
-
-      context "several values" do
-        let(:klass) { Class.new { include Extractors; object_class OpenStruct; as_currency :a => '/r/a', :b => '/r/b' } }
-
-        it "can extract the first value" do
-          instance.a.should == ten
-        end
-
-        it "can extract the second value" do
-          instance.b.should == twenty
-        end
-      end
-
-      context "missing values" do
-        let(:klass) { Class.new { include Extractors; object_class OpenStruct; as_currency :a => '/r/a', :c => '/r/c' } }
-
-        it "extracts the first value" do
-          instance.a.should == ten
-        end
-
-        it "doesn't attempt to set the second" do
-          instance.should_not respond_to(:c)
-        end
+        klass.deserialise_one(xml).thing.should == ten
       end
     end
   end
