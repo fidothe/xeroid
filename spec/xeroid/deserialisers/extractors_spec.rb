a1800a65a02a23a4fc1f10920439fa72ad6d25e7
Matt Patterson <matt@reprocessed.org>
2013-04-05 13:30:54 +0200

    Verify we can extract several values

--- spec/xeroid/deserialisers/extractors_spec.rb
+++ spec/xeroid/deserialisers/extractors_spec.rb
@@ -13,6 +13,19 @@ module Xeroid::Deserialisers
         instance = klass.deserialise_one(xml)
         instance.thing.should == "A string"
       end
+
+      context "several values" do
+        let(:klass) { Class.new { include Extractors; object_class OpenStruct; as_string :a => '/r/a', :b => '/r/b' } }
+        let(:instance) { klass.deserialise_one(xml) }
+
+        it "can extract the first value" do
+          instance.a.should == 'A string'
+        end
+
+        it "can extract the second value" do
+          instance.b.should == 'B string'
+        end
+      end
     end
   end
 end
